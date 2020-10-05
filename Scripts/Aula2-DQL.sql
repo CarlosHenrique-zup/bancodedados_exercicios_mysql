168727c364bcbe397ca03e2b1f6d4481845de597
CarlosHenrique-zup <carlos.henrique@zup.com.br>
2020-10-05 18:41:07 -0300

    Script - Aula2

--- Scripts/Aula2-DQL.sql
+++ Scripts/Aula2-DQL.sql
@@ -38,44 +38,57 @@ values
 (38180000, "Araxá", 100000, 0, "mg", 70000, '1444-11-19'),
 (21000000, "Goiânia", 4000000, 0, "go", 10000, '1721-07-04');
 
+/*Selecione o nome e a renda per capita das cidades com mais de 5000 habitantes*/
 SELECT c.nome AS Nome, c.renda_per_capita AS RendaPerCapita
 FROM cidade c
 WHERE c.nro_habitantes > 5000;
 
+/*Selecione todos os dados das cidades que foram fundadas depois de 12 março de 1432.*/
 SELECT c.*
 FROM cidade c
 WHERE c.data_fundacao > '1432-03-12';
 
-SELECT c.nome
+/*Selecione a cidade cujo cep é 38180000.*/
+SELECT c.nome AS Nome
 FROM cidade c
 WHERE c.cep = 38180000;
 
-SELECT c.nome
+/*Selecione o nome das cidades do estado de São Paulo.*/
+SELECT c.nome AS Nome
 FROM cidade c
 WHERE c.estado = 'sp';
 
+/*Selecione todos os dados das cidades que ficam no estado de São Paulo e que possuam mais
+do que 100 mil habitantes.*/
 SELECT  c.*
 FROM cidade c
 WHERE c.estado = 'sp' AND nro_habitantes > 100000;
 
-SELECT c.nome
-FROM cidades c
-WHERE c.renda_per_capita > 30000;
+/*Selecione o nome das cidades que são capitais e tem mais do que 30000 de renda per capita.*/
+SELECT c.nome AS Nome
+FROM cidade c
+WHERE c.capital = 1 AND c.renda_per_capita > 30000;
 
+/*Selecione o nome da cidade e o nome do estado cuja sigla é sp.*/
 SELECT c.nome as NomeCidade, e.nome as NomeEstado
 FROM estado e, cidade c
-WHERE e.sigla = 'sp';
+WHERE c.estado = e.sigla AND e.sigla = 'sp';
 
-
-SELECT c.regiao AS RegiaoCidade, e.nome AS NomeEstado,c.nome AS NomeCidade, c.nro_habitantes AS NumeroHabitantesCidade
+/*Selecione a região, o nome do estado, o nome da cidade e a quantidade de habitantes das
+cidades com renda per capita maior do que 20000 e que não são capitais.*/
+SELECT e.regiao AS RegiaoCidade, e.nome AS NomeEstado,c.nome AS NomeCidade, c.nro_habitantes AS NumeroHabitantesCidade
 FROM cidade c, estado e
 WHERE e.sigla = c.estado
 AND c.renda_per_capita > 20000 AND c.capital <> 1;
 
+/*Selecione todos os dados das cidades que pertencem a região sudeste.*/
 SELECT e.*
 FROM estado e
 WHERE e.regiao = 'Sudeste';
 
+/*Selecione o nome e a quantidade de habitantes das cidades da região sudeste que não sejam
+capitais, que não sejam do estado cuja sigla é mg e que tenham a renda per capita maior do
+que 15000.*/
 SELECT c.nome AS NomeCidade , c.nro_habitantes AS NumeroHabitantes
 FROM cidade c, estado e
 WHERE e.sigla = c.estado 
@@ -83,7 +96,6 @@ AND e.regiao = 'Sudeste'
 AND c.capital <> 1 
 AND e.sigla = 'mg' 
 AND c.renda_per_capita > 15000;
-
 
 SELECT * FROM cidade;
 SELECT * FROM estado;
